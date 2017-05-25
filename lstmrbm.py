Updated Weight Initialization; Tried to Fix Numerical Errors

lstmrbm.py
@@ -1,3 +1,4 @@
+import time
 from tqdm import tqdm
 import numpy as np
 import tensorflow as tf
@@ -125,6 +126,18 @@ def build_lstmrbm(n_hidden, n_hidden_recurrent):
         return [i + 1, k, v_t, u_t, q_t, c_t, music]
     
     
+    def get_cross_entropy(v_t, bv_t):
+        """
+        Returns cross-entropy loss.
+        """
+        epsilon = 1e-6
+        
+        term1 = v_t * tf.log(epsilon + tf.sigmoid(bv_t))
+        term2 = (1 - v_t) * tf.log(1 + epsilon - tf.sigmoid(bv_t))
+
+        return tf.reduce_mean(tf.reduce_sum(-term1 - term2, 1))
+
+    
     def generate_music(num_timesteps, x=x, music_init=music, n_visible=n_visible,
                        lstm_args=(u0, q0, c0), start_length=200):
         """
@@ -171,7 +184,10 @@ def build_lstmrbm(n_hidden, n_hidden_recurrent):
     # Get free energy cost
     cost = get_free_energy_cost(x, W, batch_bv_t, batch_bh_t, k=15)
     
-    return x, cost, generate_music, params, u0, q0, c0, lr, music
+    # Get cross-entropies for initialization and monitoring
+    cross_entropy = get_cross_entropy(x, batch_bv_t)
+
+    return x, cost, cross_entropy, generate_music, params, u0, q0, c0, lr, music
 
 
 class LSTM_RBM:
@@ -183,7 +199,7 @@ class LSTM_RBM:
         """
         Constructs a RNN-RBM with training and sequence generation functions.
         """
-        self.x, cost, generate, params, u0, q0, c0, \
+        self.x, self.cost, self.cross_entropy, generate, params, u0, q0, c0, \
             self.learning_rate, self.music = build_lstmrbm(n_hidden, n_hidden_recurrent)
 
         self.n_epochs = n_epochs
@@ -191,13 +207,12 @@ class LSTM_RBM:
 
         # Variables for training
         self.training_vars = params + [u0, q0, c0] 
-        self.cost = cost
         
         opt_func = tf.train.AdamOptimizer(learning_rate = self.learning_rate)
-        gradients = opt_func.compute_gradients(cost, self.training_vars)
+        gradients = opt_func.compute_gradients(self.cost, self.training_vars)
         
         # Clips gradients to prevent 
-        gradients = [(tf.clip_by_value(grad, -10., 10.), var) 
+        gradients = [(tf.clip_by_value(grad, -1., 1.), var) 
                      for grad, var in gradients]
 
         self.update = opt_func.apply_gradients(gradients) # Update step
@@ -210,22 +225,45 @@ class LSTM_RBM:
         Initialize the RBM weights from Contrastive Divergence
         """
         W, bv, bh = self.training_vars[:3]
+        
         rbm_update = cd_update(self.x, W, bv, bh, 1, self.learning_rate)
         
+        opt_func = tf.train.AdamOptimizer(learning_rate = self.learning_rate)
+        gradients = opt_func.compute_gradients(self.cross_entropy, self.training_vars)
+        
+        # Clip gradients, need to check for None this time
+        gradients = [
+            (None, var) if grad is None else (tf.clip_by_value(grad, -1., 1.), var)
+            for grad, var in gradients]
+        rnn_update = opt_func.apply_gradients(gradients)
+
         saver = tf.train.Saver()
         with tf.Session() as sess:
             init = tf.global_variables_initializer()
             sess.run(init)
 
             for epoch in range(self.n_epochs):
+                entropies = []
                 for song in songs:
                     for i in range(1, len(song), self.batch_size):
+                        alpha = min(0.001, 0.1/float(i))
+
+                        # Update RBM parameters using CD
                         batch = song[i: i + self.batch_size]
                         sess.run(rbm_update, feed_dict={self.x: batch,
-                                                        self.learning_rate:5e-3})
+                                                        self.learning_rate:alpha})
                         
-            save_path = saver.save(sess, save)
+                        # Initialize the RNN weights by minimizing cross-entropy; better captures temporal
+                        # dependicies
+                        _, cross_entropy = sess.run([rnn_update, self.cross_entropy], feed_dict={self.x:batch, 
+                                                                                                 self.learning_rate:alpha})
+                     
+                        entropies.append(cross_entropy)
 
+                print("Initialization Epoch: {}/{}. Cross Entropy: {}".format(epoch, self.n_epochs,
+                                                                                      np.mean(entropies)))
+            save_path = saver.save(sess, save)
+            
 
     def fit(self, songs, 
             checkpoint="parameter_checkpoints/lstmrbm_initial.ckpt", 
@@ -244,15 +282,20 @@ class LSTM_RBM:
                 
             for epoch in range(self.n_epochs):
                 costs = []
+                entropies = []
                 for song in songs:
                     for i in range(1, len(song), self.batch_size):
                         batch = song[i: i + self.batch_size]
                         # Adaptive learning rate
-                        alpha = min(0.01, 0.1/float(i))
-                        _, cost = sess.run([self.update, self.cost], feed_dict={self.x:batch, 
-                                                                                self.learning_rate:alpha})
+                        alpha = min(0.001, 0.1/float(i))
+                        _, cost, cross_entropy  = sess.run([self.update, self.cost, self.cross_entropy], 
+                                                           feed_dict={self.x:batch, 
+                                                                      self.learning_rate:alpha})
                         costs.append(cost)
-                print("Epoch: {}/{}. Cost: {}".format(epoch, self.n_epochs, np.mean(costs)))
+                        entropies.append(cross_entropy)
+                        
+                        print("Epoch: {}/{}. Free Energy: {}. Cross Entropy: {}".format(epoch, self.n_epochs, 
+                                                                                        np.mean(costs), np.mean(entropies)))
                 
                 if (epoch + 1) % 50 == 0:
                     saver.save(sess, "parameter_checkpoints/lstmrbm_epoch_{}.ckpt".format(epoch + 1))

lstmrbm_train.py
@@ -2,11 +2,10 @@ from lstmrbm import *
 from midi_parser import *
 
 if __name__=='__main__':
-    songs = get_songs('Test_Midi')
+    #songs = get_songs('Test_Midi')
     
-    '''
     songs = []
-    for file in glob.glob("Jazz_Data/*_melody.txt"):
+    for file in glob.glob("Classical_Data/*.txt")[:5]:
         song = np.genfromtxt(file)
         try:
             song = song[:int(np.floor((song.shape[0]/num_timesteps) * num_timesteps))]
@@ -16,8 +15,7 @@ if __name__=='__main__':
                 songs.append(song)
         except:
             continue
-    '''
-
+        
     print('{} songs processed'.format(len(songs)))
     
     # Hyperparameter

rbm.py
@@ -8,8 +8,8 @@ def sample(probs):
     """
     return tf.floor(probs + tf.random_uniform(tf.shape(probs), 0, 1))
 
-
-def gibbs_sample(x, W, bv, bh, k):
+    
+def gibbs_sample(x, W, bv, bh, k, keep_prob=0.5):
     """
     Runs Gibbs Sampling for k steps from the probability distribution of the RBM defined by W, bh, bv
     """
@@ -17,10 +17,17 @@ def gibbs_sample(x, W, bv, bh, k):
         """
         Run a single step
         """
-        #Propagate the visible values to get the hidden values
-        h_k = sample(tf.sigmoid(tf.matmul(x_k, W) + bh))
-        #Propagate the hidden values to get the visible values
-        x_k = sample(tf.sigmoid(tf.matmul(h_k, tf.transpose(W)) + bv))
+        # Apply dropout
+        x_drop_out = tf.nn.dropout(x_k, keep_prob)
+
+        # Propagate the visible values to get the hidden values
+        h_k = sample(tf.sigmoid(tf.matmul(x_drop_out, W) + bh))
+        # Apply dropout
+        h_drop_out = tf.nn.dropout(h_k, keep_prob)
+
+        # Propagate the hidden values to get the visible values
+        x_k = sample(tf.sigmoid(tf.matmul(h_drop_out, tf.transpose(W)) + bv))
+        
         return i + 1, k, x_k
     
     i = tf.constant(0) #counter
@@ -34,15 +41,21 @@ def gibbs_sample(x, W, bv, bh, k):
     return x_sample
 
 
-def gibbs_sample_converge(x, W, bv, bh):
+def gibbs_sample_converge(x, W, bv, bh, keep_prob=0.5):
     """
     Run Gibbs Sampling until convergence on RBM.
     """
     def step(x, stop_condition):
         x_prev = x
+        
+        # Apply dropout
+        x_drop_out = tf.nn.dropout(x, keep_prob)
         #Propagate the visible values to sample the hidden values
-        h_k = sample(tf.sigmoid(tf.matmul(x, W) + bh))
+        h_k = sample(tf.sigmoid(tf.matmul(x_drop_out, W) + bh))
+        
+        
         #Propagate the hidden values to sample the visible values
+        h_drop_out = tf.nn.dropout(h_k, keep_prob)
         x = tf.sigmoid(tf.matmul(h_k, tf.transpose(W)) + bv)
             
         # Convergence of probability vectors
@@ -96,11 +109,15 @@ def get_free_energy_cost(x, W, bv, bh, k):
     """
     #First, draw a sample from the RBM
     x_sample = gibbs_sample(x, W, bv, bh, k)
-
+    
     def free_energy(v):
         #The function computes the free energy of a visible vector. 
-        return (-tf.reduce_sum(tf.log(1 + tf.exp(tf.matmul(v, W) + bh)), 1) 
-                 - tf.matmul(v, tf.transpose(bv)))
+        wv_b = tf.matmul(v, W) + bh
+        hidden_term = tf.reduce_sum(tf.log(1 + tf.exp(wv_b)), 1)
+        vbias_term = tf.matmul(v, tf.transpose(bv))
+        
+        return -hidden_term - vbias_term
+        
 
     #The cost is based on the difference in free energy between x and xsample
     cost = tf.reduce_mean(tf.sub(free_energy(x), free_energy(x_sample)))

rnnrbm.py
@@ -1,6 +1,7 @@
 from tqdm import tqdm
 import numpy as np
 import tensorflow as tf
+import time
 
 from rbm import * 
 from tensorflow.contrib import rnn
@@ -50,15 +51,18 @@ def build_rnnrbm(n_hidden, n_hidden_recurrent):
         
         u_t = (tf.tanh(bu + tf.matmul(v_t, Wvu) + tf.matmul(u_tm1, Wuu)))
         return u_t
+
     
     def bv_recurrence(bv_t, u_tm1):
         bv_t = tf.add(bv, tf.matmul(u_tm1, Wuv))
         return bv_t
 
+
     def bh_recurrence(bh_t, u_tm1):
         bh_t = tf.add(bh, tf.matmul(u_tm1, Wuh))
         return bh_t       
         
+
     def recurrence(i, k, v_tm1, u_tm1, music):
         #Get the bias vectors from the current state of the RNN
         bv_t = tf.add(bv, tf.matmul(u_tm1, Wuv))
@@ -74,7 +78,19 @@ def build_rnnrbm(n_hidden, n_hidden_recurrent):
         music = tf.concat(0, [music, v_t])
         return i + 1, k, v_t, u_t, music
     
-    
+
+    def get_cross_entropy(v_t, bv_t):
+        """
+        Returns cross-entropy loss.
+        """
+        epsilon = 1e-6
+        
+        term1 = v_t * tf.log(epsilon + tf.sigmoid(bv_t))
+        term2 = (1 - v_t) * tf.log(1 + epsilon - tf.sigmoid(bv_t))
+
+        return tf.reduce_mean(tf.reduce_sum(-term1 - term2, 1))
+               
+ 
     def generate_music(num_timesteps, x=x, music_init=music, n_visible=n_visible,
                        u0=u0, start_length=200):
         """
@@ -83,10 +99,7 @@ def build_rnnrbm(n_hidden, n_hidden_recurrent):
         batch_u = tf.scan(u_recurrence, x, initializer=u0)
 
         # Round down and get starting sequence
-        if midi_parser.num_timesteps > 1:
-            batch_u[int(np.floor(start_length/midi_parser.num_timesteps)), :, :]
-        else:
-            u_init =  batch_u[start-length - 1, :, :]
+        u_init = batch_u[int(np.floor(start_length/midi_parser.num_timesteps)), :, :]
         
         i = tf.constant(1, tf.int32)
         k = tf.constant(num_timesteps)
@@ -98,6 +111,7 @@ def build_rnnrbm(n_hidden, n_hidden_recurrent):
         
         return music
         
+
     
     # Reshape biases to match batch_size
     tf.assign(batch_bh_t, tf.tile(batch_bh_t, [batch_size, 1]))
@@ -114,7 +128,11 @@ def build_rnnrbm(n_hidden, n_hidden_recurrent):
     # Get free energy cost
     cost = get_free_energy_cost(x, W, batch_bv_t, batch_bh_t, k=15)
     
-    return x, cost, generate_music, W, bh, bv, lr, Wuh, Wuv, Wvu, Wuu, bu, u0, music
+    # Get cross-entropies for initialization and monitoring
+    cross_entropy = get_cross_entropy(x, batch_bv_t)
+    
+    return x, cost, cross_entropy, generate_music, W, bh, bv, lr, Wuh, Wuv, Wvu, Wuu, bu,\
+        u0, music
 
 
 class RNN_RBM:
@@ -122,11 +140,11 @@ class RNN_RBM:
     Class to train an RNN-RBM from MIDI and generate music.
     """
     def __init__(self, n_hidden=150, n_hidden_recurrent=100, 
-                 batch_size=100, n_epochs=500):
+                 batch_size=100, n_epochs=400):
         """
         Constructs a RNN-RBM with training and sequence generation functions.
         """
-        self.x, cost, generate, W, bh, bv, self.learning_rate, \
+        self.x, self.cost, self.cross_entropy, generate, W, bh, bv, self.learning_rate, \
             Wuh, Wuv, Wvu, Wuu, bu, u0, self.music = build_rnnrbm(n_hidden, n_hidden_recurrent)
         
         self.n_epochs = n_epochs
@@ -134,13 +152,12 @@ class RNN_RBM:
 
         # Variables for training
         self.training_vars = [W, bh, bv, Wuh, Wuv, Wvu, Wuu, bu, u0] 
-        self.cost = cost
         
         opt_func = tf.train.AdamOptimizer(learning_rate = self.learning_rate)
-        gradients = opt_func.compute_gradients(cost, self.training_vars)
+        gradients = opt_func.compute_gradients(self.cost, self.training_vars)
         
         # Clips gradients to prevent 
-        gradients = [(tf.clip_by_value(grad, -10., 10.), var) 
+        gradients = [(tf.clip_by_value(grad, -1., 1.), var) 
                      for grad, var in gradients]
         
         self.update = opt_func.apply_gradients(gradients) # Update step
@@ -150,26 +167,50 @@ class RNN_RBM:
         
     def initialize_weights(self, songs, save="parameter_checkpoints/rnnrbm_initial.ckpt"):
         """
-        Initialize the RBM weights from Contrastive Divergence
+        Initialize the RBM weights from Contrastive Divergence, and RNN weights using
+        standard SGD on cross-entropy loss.
         """
-        W, bh, bv = self.training_vars[:3]
+        W, bh, bv, Wuh, Wuv, Wvu, Wuu, bu, u0 = self.training_vars
+        
         rbm_update = cd_update(self.x, W, bv, bh, 1, self.learning_rate)
                 
+        
+        opt_func = tf.train.AdamOptimizer(learning_rate = self.learning_rate)
+        gradients = opt_func.compute_gradients(self.cross_entropy, [Wvu, Wuu, bu, u0, Wuv, bv])
+        gradients = [(tf.clip_by_value(grad, -1., 1.), var) 
+                     for grad, var in gradients]
+        rnn_update = opt_func.apply_gradients(gradients)
+                        
         saver = tf.train.Saver()
         with tf.Session() as sess:
             init = tf.global_variables_initializer()
             sess.run(init)
-
-            for epoch in range(self.n_epochs):
+            
+            for epoch in range(100):
+                start = time.time()
+                entropies = []
                 for song in songs:
                     for i in range(1, len(song), self.batch_size):
+                        alpha = min(0.001, 0.1/float(i))
+                        
+                        # Update RBM parameters using CD
                         batch = song[i: i + self.batch_size]
                         sess.run(rbm_update, feed_dict={self.x: batch,
-                                                        self.learning_rate:5e-3})
+                                                        self.learning_rate:alpha})
                         
+                        # Initialize the RNN weights by minimizing cross-entropy; better captures temporal
+                        # dependicies
+                        _, cross_entropy = sess.run([rnn_update, self.cross_entropy], feed_dict={self.x:batch, 
+                                                                                                 self.learning_rate:alpha})
+                        
+                        entropies.append(cross_entropy)
+
+                print("Initialization Epoch: {}/{}. Cross Entropy: {}. Time: {}".format(epoch, self.n_epochs,
+                                                                                        np.mean(entropies), time.time()-start))
+                
             save_path = saver.save(sess, save)
 
-
+            
     def fit(self, songs, 
             checkpoint="parameter_checkpoints/rnnrbm_initial.ckpt", 
             save="parameter_checkpoints/rnnrbm_final.ckpt"):
@@ -186,16 +227,23 @@ class RNN_RBM:
                 saver.restore(sess, checkpoint)
                 
             for epoch in range(self.n_epochs):
+                start = time.time()
                 costs = []
+                entropies = []
                 for song in songs:
                     for i in range(1, len(song), self.batch_size):
                         batch = song[i: i + self.batch_size]
                         # Adaptive learning rate
                         alpha = min(0.01, 0.1/float(i))
-                        _, cost = sess.run([self.update, self.cost], feed_dict={self.x:batch, 
-                                                                                self.learning_rate:alpha})
+                        _, cost, cross_entropy = sess.run([self.update, self.cost, self.cross_entropy], 
+                                                          feed_dict={self.x:batch, 
+                                                                     self.learning_rate:alpha})
                         costs.append(cost)
-                print("Epoch: {}/{}. Cost: {}".format(epoch, self.n_epochs, np.mean(costs)))
+                        entropies.append(cross_entropy)
+                        
+                print("Epoch: {}/{}. Free Energy: {}. Cross Entropy: {}. Time: {}".format(epoch, self.n_epochs,
+                                                                                          np.mean(costs), np.mean(entropies),
+                                                                                          time.time() - start))
                 
                 if (epoch + 1) % 50 == 0:
                     saver.save(sess, "parameter_checkpoints/rnnrbm_epoch_{}.ckpt".format(epoch + 1))

rnnrbm_train.py
@@ -11,11 +11,10 @@ if __name__=='__main__':
     # Uncomment the method you use
 
     
-    songs = get_songs('Test_Midi')
+    #songs = get_songs('Test_Midi')
     
-    '''
     songs = []
-    for file in glob.glob("Jazz_Data/*_melody.txt"):
+    for file in glob.glob("Classical_Data/*txt")[:50]:
         song = np.genfromtxt(file)
         try:
             song = song[:int(np.floor((song.shape[0]/num_timesteps) * num_timesteps))]
@@ -25,16 +24,15 @@ if __name__=='__main__':
                 songs.append(song)
         except:
             continue
-    '''
-
+    
     print('{} songs processed'.format(len(songs)))
     
     # Hyperparameter
     n_hidden = 150
     n_hidden_recurrent = 100
     batch_size = 100
-    n_epochs = 200
-
+    n_epochs = 400
+    
     model = RNN_RBM(n_hidden, n_hidden_recurrent, batch_size, n_epochs)
     
     model.initialize_weights(songs)